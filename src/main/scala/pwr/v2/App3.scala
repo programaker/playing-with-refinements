bcac2ff3946b74fd4875e12249651c70c9ea2498
programaker <programaker@gmail.com>
2018-03-12 23:05:16 -0300

    Better print errors

--- src/main/scala/pwr/v2/App3.scala
+++ src/main/scala/pwr/v2/App3.scala
@@ -1,13 +1,17 @@
 package pwr.v2
 
 import cats.data.ValidatedNel
+import cats.data.Validated.{Valid, Invalid}
 import cats.implicits._
 import eu.timepit.refined._
 import pwr.v2.SpBus._
 
 object App3 {
   def main(args: Array[String]): Unit = {
-    println(buildBus(id = "0", line = "SV917", company = ""))
+    buildBus(id = "0", line = "SV917", company = "") match {
+      case Valid(bus) => println(bus)
+      case Invalid(errors) => errors.toList.foreach(println)
+    }
   }
 
   private def buildBus(id: String, line: String, company: String): ValidatedNel[String, SpBus] = {
